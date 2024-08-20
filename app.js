af09a646050daed9450f4fb39818baf7fae1df90
Andres Martinez <amartinezcarrillo1006@gmail.com>
2024-08-19 21:14:44 -0300

    Arregle bug de el encriptador

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
 function encriptar() {
-  let texto = document.getElementById("texto").value; 
+  let texto = document.getElementById("texto").value;
   let textoCifrado = texto
-    .replaceAll("a","ai")
-    .replaceAll("e","enter")
-    .replaceAll("i","imes")
-    .replaceAll("o","ober")
-    .replaceAll("u","ufat");
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
 
-  if(texto.length!=0){
-    let textoDom= document.getElementById("textoDom");
-    textoDom.innerHTML= `${textoCifrado}`;
-    let fotoDom= document.getElementById("fotoDom");
+  if (texto.length != 0) {
+    let textoDom = document.getElementById("textoDom");
+    textoDom.innerHTML = `${textoCifrado}`;
+    let fotoDom = document.getElementById("fotoDom");
     fotoDom.style.display = "none";
-    let parrafoDom= document.getElementById("elementoAEliminar");
-      parrafoDom.innerText= ' '
+    let parrafoDom = document.getElementById("elementoAEliminar");
+    parrafoDom.innerText = " ";
     console.log(textoCifrado);
-    
-
-
-  }else{
-    let textoDom= document.getElementById("textoDom");
-    textoDom.innerHTML= `Ningún mensaje fue encontrado`;
-    let parrafoDom= document.getElementById("elementoAEliminar");
-    parrafoDom.innerText= 'Ingresa el texto que desees encriptar o desencriptar.';
+  } else {
+    let textoDom = document.getElementById("textoDom");
+    textoDom.innerHTML = `Ningún mensaje fue encontrado`;
+    let parrafoDom = document.getElementById("elementoAEliminar");
+    parrafoDom.innerText =
+      "Ingresa el texto que desees encriptar o desencriptar.";
   }
-
-
-
-
-
 }
 
 function desencriptar() {
-  let texto = document.getElementById("texto").value; 
+  let texto = document.getElementById("texto").value;
   let textoDescifrado = texto
-    .replaceAll("ai","a")
-    .replaceAll("enter","e")
-    .replaceAll("imes","i")
-    .replaceAll("ober","o")
-    .replaceAll("ufat","u");
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
 
-    if(texto.length!=0){
-      let textoDom= document.getElementById("textoDom");
-      textoDom.innerHTML= `${textoDescifrado}`;
-      let parrafoDom= document.getElementById("elementoAEliminar");
-      parrafoDom.innerText= ' '
-      let fotoDom= document.getElementById("fotoDom");
-      fotoDom.style.display = "none";
-  
-      console.log(textoCifrado);
-      
-  
-  
-    }else{
-      let textoDom= document.getElementById("textoDom");
-      textoDom.innerText= `Ningún mensaje fue encontrado`;
-      let parrafoDom= document.getElementById("elementoAEliminar");
-      parrafoDom.innerText= 'Ingresa el texto que desees encriptar o desencriptar.'
+  if (texto.length != 0) {
+    let textoDom = document.getElementById("textoDom");
+    textoDom.innerHTML = `${textoDescifrado}`;
+    let parrafoDom = document.getElementById("elementoAEliminar");
+    parrafoDom.innerText = " ";
+    let fotoDom = document.getElementById("fotoDom");
+    fotoDom.style.display = "none";
+
+    console.log(textoCifrado);
+  } else {
+    let textoDom = document.getElementById("textoDom");
+    textoDom.innerText = `Ningún mensaje fue encontrado`;
+    let parrafoDom = document.getElementById("elementoAEliminar");
+    parrafoDom.innerText =
+      "Ingresa el texto que desees encriptar o desencriptar.";
   }
-  
+}
 
 
 
+
+function copiarTexto() {
+  let textarea = document.getElementById("textoDom");
+  textarea.select();
+  textarea.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+  document.execCommand("copy");
 }
